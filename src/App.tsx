b507f350c31a3bd961f9fbe5279e9cdd2a9c4a3b
Joakim Östelid <joakim.ostelid@outlook.com>
2023-01-22 13:09:47 +0100

    staet

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
-  return (
-    <div>
-     App works!
-    </div>
-  )
+  return <div>App works!</div>;
 }
 
-export default App
+export default App;
