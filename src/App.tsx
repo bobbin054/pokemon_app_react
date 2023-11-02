46cae5acb533ae7356d1715a7061247d3cbe14b2
Joakim Östelid <joakim.ostelid@outlook.com>
2023-11-02 20:48:00 +0100

    refac

--- src/App.tsx
+++ src/App.tsx
@@ -3,53 +3,13 @@ import { useState } from "react";
 import PokemonTable from "./PokemonTable";
 import Pagenation from "./Pagination";
 import useSWR from "swr";
-
-const POKE_API = "https://pokeapi.co/api/v2/pokemon?limit=5";
-
-async function fetcher(endpoint: string) {
-  const json = await (await fetch(endpoint)).json();
-  // Recursively check for URL properties and make requests on those URLs
-  const checkForUrls = async (obj: any) => {
-    const promises = [];
-    for (const key in obj) {
-      if (key === "url") {
-        promises.push(
-          (async () => {
-            obj.data = await (await fetch(obj[key])).json();
-            delete obj.url;
-          })()
-        );
-      } else if (typeof obj[key] === "object" && obj[key] !== null) {
-        promises.push(checkForUrls(obj[key]));
-      }
-    }
-    await Promise.all(promises);
-  };
-  await checkForUrls(json);
-  // console.log("json:", json);
-  return json;
-}
+import { PokeNode } from "./PokeNode";
+import { MyPokemonFetcher } from "./MyPokemonFetcher";
 
 function App() {
-  const [currentPageUrl, setCurrentPageUrl] = useState(POKE_API);
-  const { data, error } = useSWR(currentPageUrl, fetcher);
-  console.log("data:", data);
-  if (error) return <pre>Error: {JSON.stringify(error, null, 2)}</pre>;
-
-  function gotoNextPage() {
-    setCurrentPageUrl(data.next);
-  }
-
-  function gotoPrevPage() {
-    setCurrentPageUrl(data.previous);
-  }
-
   return (
     <>
-      <h1>List of Pokemon</h1>
-      <div>Current URL: {currentPageUrl}</div>
-      {data?.results && <PokemonTable pokemon={data?.results} />}
-      <Pagenation gotoNextPage={gotoNextPage} gotoPrevPage={data?.previous && gotoPrevPage}></Pagenation>
+      <MyPokemonFetcher></MyPokemonFetcher>
     </>
   );
 }
